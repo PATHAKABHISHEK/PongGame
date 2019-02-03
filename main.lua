dde3ca7275adccd2c46c45c2d03c57909bb43c17
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-02-03 16:20:16 +0530

    window is set up using push methods

--- main.lua
+++ main.lua
@@ -1,16 +1,31 @@
 -- This is the File which contains code to run by default
 
---Setting up the window width and height in pixels
+-- Reguiring/Importing push.lua file
+push = require 'push' -- It will handle the graphics sizes in the game
+
+-- Setting up the window width and height in pixels
 WINDOW_HEIGHT = 720
 WINDOW_WIDTH = 1280
+
+VIRTUAL_HEIGHT = 243
+VIRTUAL_WIDTH = 432
+
 
 -- This is the function which will be called when game will be loaded
 -- first time
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- As now we are using push so we are not using setMode method
+    --[[ love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,-- fullscreen mode cannot be accesed
         resizable = false, -- window will not be resized 
         vsync = true --this is for synching of monitors
+    }) ]]
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, 
+    WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
     })
 end
 
