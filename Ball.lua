0ccb154f642d0db14d5af11f865c4752ec94230c
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-02-03 19:19:06 +0530

    Added Collidng Functionality in Ball

--- Ball.lua
+++ Ball.lua
@@ -23,6 +23,21 @@ function Ball:reset()
     self.speed_y = math.random(-50,50)  
 end
 
+function Ball:collides(paddle)
+    
+    if self.x > paddle.x + paddle.width or 
+    paddle.x > self.x + self.width then
+        return false
+    end
+
+    if self.y > paddle.y + paddle.height or 
+    paddle.y > self.y + self.height then
+        return false
+    end 
+
+    return true 
+end
+
 function Ball:render()
     love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
 end
