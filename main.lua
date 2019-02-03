a17ae493046f397c1fac25ab8f61c83456f33e89
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-02-03 16:08:14 +0530

    Added  'pong game' text

--- main.lua
+++ main.lua
@@ -7,11 +7,20 @@ WINDOW_WIDTH = 1280
 -- This is the function which will be called when game will be loaded
 -- first time
 function love.load()
-    love.window.setmode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,-- fullscreen mode cannot be accesed
         resizable = false, -- window will not be resized 
         vsync = true --this is for synching of monitors
     })
 end
 
+function love.draw()
+    love.graphics.printf(
+        'Pong Game',
+        0,
+        WINDOW_HEIGHT/2 - 6,
+        WINDOW_WIDTH,
+        'center'
+    )
+end
 
